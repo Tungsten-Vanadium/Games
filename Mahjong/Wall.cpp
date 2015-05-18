7300bacbb8eda9e55420adc996f8202ef54ab934
Vincent Tong <tongvs@miamioh.edu>
2015-05-18 15:30:41 -0400

    Fully implement Wall.cpp
    Fix indentation. Implement display().

--- Mahjong/Wall.cpp
+++ Mahjong/Wall.cpp
@@ -18,16 +18,16 @@ Wall::~Wall() {
 }
 
 void Wall::fillWall() {
-  std::list<Tile> tempWall;
-  for(int suit = 1; suit <= 4; suit++) {
-    for(int num = 1; num <= 9; num++) {
-      tempWall.add(new Tile(suit, num));
-    }
-  }
-  srand (time(NULL));
-  for(int tile = 0; tile < 144; tile++) {
-    wall.add(tempWall.erase(rand % tempWall.size()));
-  }
+	std::list<Tile> tempWall;
+	for(int suit = 1; suit <= 4; suit++) {
+		for(int num = 1; num <= 9; num++) {
+			tempWall.add(new Tile(suit, num));
+		}
+	}
+	srand (time(NULL));
+	for(int tile = 0; tile < 144; tile++) {
+		wall.add(tempWall.erase(rand % tempWall.size()));
+	}
 }
 
 int Wall::getSize() {
@@ -35,13 +35,19 @@ int Wall::getSize() {
 }
 
 std::string Wall::display() {
-
+	for(int i = 0; i < 144; i++) {
+		std::list<Tile> tempWall;
+		Tile disp = wall.pop_front();
+		std::cout << disp.toString();
+		tempWall.push_back(disp);
+	}
+	wall = tempWall;
 }
 
 Tile Wall::drawFromWall(bool front) {
-  if(front == true) {
-    return wall.pop_front();
-  }else{
-    return wall.pop_back();
-  }
+	if(front == true) {
+		return wall.pop_front();
+	}else{
+		return wall.pop_back();
+	}
 }
